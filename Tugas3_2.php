41af3bb39559788629d64e9b0eaee48c7ffa3fc0
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 10:02:19 +0700

    Add files via upload

---
+++ Tugas3_2.php
@@ -0,0 +1,10 @@
+<?php
+$awal = 150000;
+$bunga = 12.5;
+$total = $awal+(($awal*$bunga)/100);
+
+echo "Tabungan Awal = Rp. $awal <br>";
+echo "Bunga Setahun = $bunga % <br>";
+echo "Total Tabungan Setahun = ? <br>";
+echo "Jawaban : Rp. $total";
+?>
